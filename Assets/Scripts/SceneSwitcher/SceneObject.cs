20b51797da0818eab1f0e385a09f8eee55dabbf4
DESKTOP-T603PMR\pc <yozhunchia@gmail.com>
2017-05-13 21:21:44 +0800

    -Interactable Indicator Added

--- Assets/Scripts/SceneSwitcher/SceneObject.cs
+++ Assets/Scripts/SceneSwitcher/SceneObject.cs
@@ -3,14 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneObject : MonoBehaviour {
+    
+    public string objectName;
+    public MeshRenderer meshRenderer;
+    
+    public List<ObjectData> objectData;
 
-    public bool[] enableOnScene;
-    public bool disable;
+    bool disable = false;
+    int index = 0;
+    Texture hintIndicator; 
+
+    Renderer renderer;
+    GameObject player;
+
+    bool showHint = false;
+
+    [System.Serializable]    
+    public struct ObjectData
+    {
+        public bool enableOnScene;
+        public bool interactable;
+        public Material[] material;       
+    }
+
+    void Start()
+    {
+        if(player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        if(renderer == null)
+            renderer = gameObject.GetComponent<Renderer>();
+
+        if (hintIndicator == null)
+            hintIndicator = Resources.Load("UI/WhiteDot", typeof(Texture)) as Texture;
+    }
+
+    void Update()
+    {
+        ShowIndicator();
+    }
+
+    public string GetName()
+    {
+        return objectName;
+    }
+
+    public bool IsInteractable()
+    {
+        return objectData[index].interactable;
+    }
+
+    public void ShowIndicator()
+    {
+        if (objectData[index].interactable && renderer.isVisible && Vector3.Distance(player.transform.position, transform.position) < 7)
+        {
+            showHint = true;
+        }
+        else
+            showHint = false;
+    }
 
     public void AppearOnScene(int index)
     {
-        if(!disable)
-            gameObject.SetActive(enableOnScene[index]);
-    }
-	
+        this.index = index;
+
+        if (!disable)
+        {
+            gameObject.SetActive(objectData[index].enableOnScene);
+
+            for (int i = 0; i < meshRenderer.materials.Length; i++)
+            {
+                if (i <= objectData[index].material.Length - 1)
+                    meshRenderer.materials[i] = objectData[index].material[i];                
+            }
+        }
+    }    
 }
